09c928ee48d360adb4325dc699e797f94a4e4abd
Mehmet Salih Yavuz <salih.yavuz@metu.edu.tr>
2023-10-27 17:57:38 +0300

    chore: set telescope background and border color

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -7,7 +7,36 @@ return {
         require("tokyonight").setup {
             style = "night",
             transparent = true,
-            on_highlights = function(hl, c) end,
+            on_highlights = function(hl, c)
+                local prompt = "#2d3149"
+                hl.TelescopeNormal = {
+                    bg = "none",
+                    fg = c.fg_dark,
+                }
+                hl.TelescopeBorder = {
+                    bg = "none",
+                    fg = c.bg_dark,
+                }
+                hl.TelescopePromptNormal = {
+                    bg = prompt,
+                }
+                hl.TelescopePromptBorder = {
+                    bg = "none",
+                    fg = prompt,
+                }
+                hl.TelescopePromptTitle = {
+                    bg = "none",
+                    fg = prompt,
+                }
+                hl.TelescopePreviewTitle = {
+                    bg = "none",
+                    fg = c.bg_dark,
+                }
+                hl.TelescopeResultsTitle = {
+                    bg = "none",
+                    fg = c.bg_dark,
+                }
+            end,
         }
         vim.cmd [[colorscheme tokyonight]]
     end,
