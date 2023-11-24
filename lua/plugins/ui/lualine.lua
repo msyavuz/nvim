cfefdf9efe3a664a4f144859e786f94b494928d0
Mehmet Salih Yavuz <salih.yavuz@metu.edu.tr>
2023-11-24 18:24:45 +0300

    chore: add filename to lualine

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -41,6 +41,7 @@ return {
                         },
                     },
                     { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                    { "filename" },
                 },
                 lualine_x = {
           -- stylua: ignore
