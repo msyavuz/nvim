0676772a6e3877c0ea2ae0b9e032d98e0f0c3cdc
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2023-12-24 16:17:53 +0300

    chore: minor changes

--- lua/plugins/code/lint.lua
+++ lua/plugins/code/lint.lua
@@ -1,30 +1,35 @@
 local linters = {
-    lua = { "luacheck" },
-    python = { "mypy" },
-    gdscript = { "gdlint" },
-    gitcommit = { "commitlint" },
-    go = { "golangcilint" },
-    sql = { "sqlfluff" },
-    json = { "jsonlint" },
+	lua = { "luacheck" },
+	python = { "mypy" },
+	gdscript = { "gdlint" },
+	gitcommit = { "commitlint" },
+	go = { "golangcilint" },
+	sql = { "sqlfluff" },
+	json = { "jsonlint" },
 }
 
 return {
-    "mfussenegger/nvim-lint",
+	"mfussenegger/nvim-lint",
 
-    config = function()
-        require("lint").linters_by_ft = linters
+	config = function()
+		require("lint").linters_by_ft = linters
 
-        require("lint").linters.sqlfluff.args = {
-            "--dialect=sqlite",
-        }
+		require("lint").linters.sqlfluff.args = {
+			"--dialect=sqlite",
+		}
 
-        local augroup = vim.api.nvim_create_augroup("Linter", { clear = true })
+		require("lint").linters.commitlint.args = {
+			"--config",
+			"~/.config/defaults/commitlint.config.js",
+		}
 
-        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-            callback = function()
-                require("lint").try_lint()
-            end,
-            group = augroup,
-        })
-    end,
+		local augroup = vim.api.nvim_create_augroup("Linter", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+			group = augroup,
+		})
+	end,
 }
