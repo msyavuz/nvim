2c55b1188071ba46facdb7398c5830656e3b143b
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-04-08 23:05:11 +0300

    chore: change move binding for terminal agnosticism

--- lua/plugins/qol/move.lua
+++ lua/plugins/qol/move.lua
@@ -1,5 +1,19 @@
 return {
 	"echasnovski/mini.move",
 	version = false,
-	opts = {},
+	opts = {
+		mappings = {
+			-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+			left = "<S-A-h>",
+			right = "<S-A-l>",
+			down = "<S-A-j>",
+			up = "<S-A-k>",
+
+			-- Move current line in Normal mode
+			line_left = "<S-A-h>",
+			line_right = "<S-A-l>",
+			line_down = "<S-A-j>",
+			line_up = "<S-A-k>",
+		},
+	},
 }
