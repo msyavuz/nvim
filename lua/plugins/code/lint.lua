9f23339e7d6eb37ce158ccefb5b2f5c9332d3393
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-06-18 12:53:46 +0300

    refactor(lint): remove commitlint

--- lua/plugins/code/lint.lua
+++ lua/plugins/code/lint.lua
@@ -2,7 +2,7 @@ return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		require("lint").linters_by_ft = {
-			gitcommit = { "commitlint" },
+			-- gitcommit = { "commitlint" },
 			python = { "ruff" },
 		}
 
