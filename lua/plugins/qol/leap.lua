82fa24ab21680a0f5a2a8949d55ae109ce526047
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-08-27 23:10:27 +0300

    chore: cleanup

--- lua/plugins/qol/leap.lua
+++ lua/plugins/qol/leap.lua
@@ -2,7 +2,6 @@ return {
 	"ggandor/flit.nvim",
 	enabled = true,
 	keys = function()
-		---@type LazyKeys[]
 		local ret = {}
 		for _, key in ipairs({ "f", "F", "t", "T" }) do
 			ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
