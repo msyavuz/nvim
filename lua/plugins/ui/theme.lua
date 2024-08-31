37ad1ec4da04e83f2acb0a258cb2dc3ebb9ffc3a
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-08-31 15:30:08 +0300

    fix: theme config

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -6,6 +6,32 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			style = "night",
+			on_colors = function(_) end,
+			on_highlights = function(hl, c)
+				local prompt = "#2d3149"
+				hl.TelescopeNormal = {
+					bg = c.bg_dark,
+					fg = c.fg_dark,
+				}
+				hl.TelescopeBorder = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopePromptBorder = {
+					fg = prompt,
+				}
+				hl.TelescopePromptTitle = {
+					fg = prompt,
+				}
+				hl.TelescopePreviewTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopeResultsTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+			end,
 		})
 		vim.cmd([[colorscheme tokyonight]])
 	end,
