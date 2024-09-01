0d0030f905e88ab8fd211ac3cff34c5e7d3926b5
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-09-01 13:45:52 +0300

    feat(smart-open): add smart open

---
+++ lua/plugins/qol/smart-open.lua
@@ -0,0 +1,26 @@
+return {
+	"danielfalk/smart-open.nvim",
+	branch = "0.2.x",
+	config = function()
+		require("telescope").load_extension("smart_open")
+	end,
+
+	keys = {
+		{
+			"<leader><leader>",
+			function()
+				require("telescope").extensions.smart_open.smart_open({
+					cwd_only = true,
+				})
+			end,
+			desc = "Smart open",
+		},
+	},
+	dependencies = {
+		"kkharji/sqlite.lua",
+		-- Only required if using match_algorithm fzf
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+		{ "nvim-telescope/telescope-fzy-native.nvim" },
+	},
+}
