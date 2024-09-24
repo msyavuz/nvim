d8ac17a3f7b4e29a7362bb08e2714057dc2cee29
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-09-24 10:12:05 +0300

    feat(tokyonight): transparency

--- lua/plugins/ui/theme.lua
+++ lua/plugins/ui/theme.lua
@@ -1,43 +1,44 @@
 --TokyoNight
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			on_colors = function(_) end,
-			on_highlights = function(hl, c)
-				local prompt = "#2d3149"
-				hl.TelescopeNormal = {
-					bg = c.bg_dark,
-					fg = c.fg_dark,
-				}
-				hl.TelescopeBorder = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-				hl.TelescopePromptBorder = {
-					fg = prompt,
-				}
-				hl.TelescopePromptTitle = {
-					fg = prompt,
-				}
-				hl.TelescopePreviewTitle = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-				hl.TelescopeResultsTitle = {
-					bg = c.bg_dark,
-					fg = c.bg_dark,
-				}
-			end,
-			styles = {
-				keywords = { italic = false },
-				functions = { italic = false },
-				variables = { italic = false },
-			},
-		})
-		vim.cmd([[colorscheme tokyonight]])
-	end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+      transparent = true,
+      on_colors = function(_) end,
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptBorder = {
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+      styles = {
+        keywords = { italic = false },
+        functions = { italic = false },
+        variables = { italic = false },
+      },
+    })
+    vim.cmd([[colorscheme tokyonight]])
+  end,
 }
