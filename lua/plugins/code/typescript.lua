29504c22985455d0a68868dfeda4e2c2d05ba831
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-02-06 17:04:19 +0300

    chore: update and ts config

--- lua/plugins/code/typescript.lua
+++ lua/plugins/code/typescript.lua
@@ -1,5 +1,11 @@
 return {
-	"pmizio/typescript-tools.nvim",
-	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	opts = {},
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {
+    settings = {
+      tsserver_file_preferences = {
+        importModuleSpecifierPreference = "non-relative"
+      }
+    }
+  },
 }
