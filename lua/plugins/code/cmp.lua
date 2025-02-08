6de96de073a780712d5d9b53c79a6d334f34f016
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2025-02-08 10:56:18 +0300

    feat(mini.ai)

--- lua/plugins/code/cmp.lua
+++ lua/plugins/code/cmp.lua
@@ -1,150 +1,150 @@
 return {
-	-- Autocompletion
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- Snippet Engine & its associated nvim-cmp source
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets.
-				-- This step is not supported in many windows environments.
-				-- Remove the below condition to re-enable on windows.
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets:
-				--    https://github.com/rafamadriz/friendly-snippets
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-						require("luasnip").filetype_extend("heex", { "eelixir" })
-						require("luasnip").filetype_extend("elixir", { "eelixir" })
-					end,
-				},
-			},
-		},
-		"saadparwaiz1/cmp_luasnip",
+  -- Autocompletion
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    -- Snippet Engine & its associated nvim-cmp source
+    {
+      "L3MON4D3/LuaSnip",
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+          return
+        end
+        return "make install_jsregexp"
+      end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip").filetype_extend("heex", { "eelixir" })
+            require("luasnip").filetype_extend("elixir", { "eelixir" })
+          end,
+        },
+      },
+    },
+    "saadparwaiz1/cmp_luasnip",
 
-		-- Adds LSP completion capabilities
-		"hrsh7th/cmp-nvim-lsp",
+    -- Adds LSP completion capabilities
+    "hrsh7th/cmp-nvim-lsp",
 
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-		"onsails/lspkind.nvim",
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
-		luasnip.config.setup({})
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    "onsails/lspkind.nvim",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+    luasnip.config.setup({})
 
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,noinsert",
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			formatting = {
-				format = lspkind.cmp_format({
-					with_text = true,
-					menu = {
-						buffer = "[buf]",
-						nvim_lsp = "[LSP]",
-						nvim_lua = "[api]",
-						path = "[path]",
-						luasnip = "[snip]",
-						gh_issues = "[issues]",
-						tn = "[TabNine]",
-						eruby = "[erb]",
-					},
-				}),
-			},
-			preselect = "None",
-			mapping = cmp.mapping.preset.insert({
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete({}),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{
-					name = "luasnip",
-					-- entry_filter = function()
-					-- 	return not vim.bo.filetype == "oil"
-					-- end,
-				},
-				{
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          with_text = true,
+          menu = {
+            buffer = "[buf]",
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[api]",
+            path = "[path]",
+            luasnip = "[snip]",
+            gh_issues = "[issues]",
+            tn = "[TabNine]",
+            eruby = "[erb]",
+          },
+        }),
+      },
+      preselect = "None",
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        {
+          name = "luasnip",
+          -- entry_filter = function()
+          -- 	return not vim.bo.filetype == "oil"
+          -- end,
+        },
+        {
 
-					{ name = "path" },
-					{ name = "buffer" },
-				},
-				{ name = "neorg" },
-			}),
-			sorting = {
-				-- NOTE: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
-				comparators = {
-					cmp.config.compare.locality,
-					cmp.config.compare.offset,
-					cmp.config.compare.exact,
-					cmp.config.compare.score,
-					cmp.config.compare.recently_used,
+          { name = "path" },
+          { name = "buffer" },
+        },
+        { name = "neorg" },
+      }),
+      sorting = {
+        -- NOTE: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+        comparators = {
+          cmp.config.compare.locality,
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
 
-					-- copied from cmp-under, but I don't think I need the plugin for this.
-					-- I might add some more of my own.
-					function(entry1, entry2)
-						local _, entry1_under = entry1.completion_item.label:find("^_+")
-						local _, entry2_under = entry2.completion_item.label:find("^_+")
-						entry1_under = entry1_under or 0
-						entry2_under = entry2_under or 0
-						if entry1_under > entry2_under then
-							return false
-						elseif entry1_under < entry2_under then
-							return true
-						end
-					end,
+          -- copied from cmp-under, but I don't think I need the plugin for this.
+          -- I might add some more of my own.
+          function(entry1, entry2)
+            local _, entry1_under = entry1.completion_item.label:find("^_+")
+            local _, entry2_under = entry2.completion_item.label:find("^_+")
+            entry1_under = entry1_under or 0
+            entry2_under = entry2_under or 0
+            if entry1_under > entry2_under then
+              return false
+            elseif entry1_under < entry2_under then
+              return true
+            end
+          end,
 
-					cmp.config.compare.kind,
-					cmp.config.compare.sort_text,
-					cmp.config.compare.length,
-					cmp.config.compare.order,
-				},
-			},
-			experimental = {
-				ghost_text = false,
-			},
-		})
-	end,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
+      experimental = {
+        ghost_text = false,
+      },
+    })
+  end,
 }
